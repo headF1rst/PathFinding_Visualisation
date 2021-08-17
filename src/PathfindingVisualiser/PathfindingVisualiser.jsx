075cb516fc4d69d022924400f858c4520b2559b0
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2021-08-18 00:15:06 +0900

    initial commit

---
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+import "./PathfindingVisualiser.css";
+
+export default class PathfindingVisualiser extends Component 
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = { nodes: [], };
+    }
+
+    componentDidMount()
+    {
+        const nodes = [];
+        for(let row = 0; row<20; row++)
+        {
+            const currentRow = [];
+            for(let col=0; col<50; col++)
+            {
+                const currentNode = {
+                    col,
+                    row,
+                };
+                currentRow.push(currentNode);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes});
+    }
+
+    render()
+    {
+        const {nodes} = this.state;
+        console.log(nodes);
+
+        return (
+        <div className = "grid"> 
+        {nodes.map((row,col) => {
+            return(
+                <div key = {rowIdx} >
+                    {row.map((node, nodeIdx) => {
+                        return (
+                            <Node
+                                key={nodeIdx}
+                                isStart = {true}
+                                test = {"foo"}
+                                test = {"kappa"}></Node>
+                        );
+                    })}
+                    </div>
+                );
+            })}
+            </div>
+        );
+    }
+}
